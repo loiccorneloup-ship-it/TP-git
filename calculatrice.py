36934eb769d4f34328903aae3827a2323bb0a943
loïc <loic.corneloup@gmail.com>
2025-10-24 16:58:05 +0200

    Ajout de la fonction multiplication

--- calculatrice.py
+++ calculatrice.py
@@ -13,3 +13,17 @@ def soustraction(a, b):
 if __name__ == "__main__":
     print("Addition :", addition(2, 3))
     print("Soustraction :", soustraction(5, 2))
+
+    def addition(a, b):
+    return a + b
+
+def soustraction(a, b):
+    return a - b
+
+def multiplication(a, b):
+    return a * b
+
+if __name__ == "__main__":
+    print("Addition :", addition(2, 3))
+    print("Soustraction :", soustraction(5, 2))
+    print("Multiplication :", multiplication(3, 4))
