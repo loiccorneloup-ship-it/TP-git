1294308dd6c1cb25c563c15a737f101f01a5f715
loïc <loic.corneloup@gmail.com>
2025-10-24 16:56:55 +0200

    Ajout de la fonction soustraction

--- calculatrice.py
+++ calculatrice.py
@@ -3,3 +3,13 @@ def addition(a, b):
 
 if __name__ == "__main__":
     print("Résultat :", addition(2, 3))
+
+    def addition(a, b):
+    return a + b
+
+def soustraction(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print("Addition :", addition(2, 3))
+    print("Soustraction :", soustraction(5, 2))
